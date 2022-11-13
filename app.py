4030108a63053c9cf51caf55007cd0f48e97f358
Krzysztof Duleba <krzysztof.duleba@student.pk.edu.pl>
2022-11-13 11:33:24 +0100

    books_5000-5200

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ def getPagination():
     pagination = int(soup.find(class_="paginationList__info").find("span").get_text().replace(" ", ""))
     return pagination
 
-current = 5000
+current = 5200
 offset = 50
 
 pagination = current#getPagination()
