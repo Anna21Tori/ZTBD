1c5fe63c7352c23bf8c91edbc11a9b6f9ff883b7
Krzysztof Dulęba <krzysztofd119@gmail.com>
2022-11-17 14:27:00 +0100

    csv files

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ def getPagination():
     pagination = int(soup.find(class_="paginationList__info").find("span").get_text().replace(" ", ""))
     return pagination
 
-current = 3000
+current = 1550
 offset = 50
 
 pagination = current#getPagination()
---
+++ verify.py
@@ -0,0 +1,22 @@
+import pandas as pd
+from os import listdir
+from pathlib import Path
+import os
+
+path = Path("Local Files")
+
+def find_csv_filenames():
+    suffix=".csv"
+    filenames = listdir(os.getcwd())
+    return [filename for filename in filenames if filename.endswith(suffix)]
+
+def verify_length(filename):
+    file = pd.read_csv(filename)
+    length = len(file)
+    if length != 1500:
+        print(f"Number of lines: {length} in file {filename}")
+
+filenames = find_csv_filenames()
+
+for name in filenames:
+    verify_length(name)
