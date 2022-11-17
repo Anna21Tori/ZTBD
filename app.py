9925badb7b7c770d8ff34fe32519fa371c5a033c
Krzysztof Dulęba <krzysztofd119@gmail.com>
2022-11-17 12:57:16 +0100

    csv files

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ def getPagination():
     pagination = int(soup.find(class_="paginationList__info").find("span").get_text().replace(" ", ""))
     return pagination
 
-current = 10000
+current = 11800
 offset = 50
 
 pagination = current#getPagination()
